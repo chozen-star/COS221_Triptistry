69a574549f8f4547a1a60623e8b67e5f81d144f4
Showlon <u25502469@tuks.co.za>
2026-05-25 21:17:35 +0200

    PHP defines the credentials to use the database.

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_PORT', getenv('DB_PORT') ?: '3307');
+define('DB_NAME', getenv('DB_NAME') ?: 'tripistry');
+define('DB_USER', getenv('DB_USER') ?: 'root');
+define('DB_PASS', getenv('DB_PASS') ?: '');
+?>
